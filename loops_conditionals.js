9a7beecc99bb7ac9089beaa3874120c2804081d8
gapinska <gapinska.mon@gmail.com>
2019-08-20 14:17:57 +0200

    loops conditionals task

---
+++ loops_conditionals.js
@@ -0,0 +1,13 @@
+// TODO: 1. stworz zmienna, nastepnie stworz instrukcje, ktora
+// wyswietli na ekranie 1, jesli zmienna jest rowna 1
+// 2, jesli zmienna jest rowna "1"
+// wyswietli ta zmienna, jesli zmienna jest rowna 2
+// 0 w pozostalych przypadkach 
+
+
+let number
+
+if(number===1 || number===2){
+    console.log(number)
+}else {console.log(0)}
+
