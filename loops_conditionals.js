169a28cf3fd766fb4907368ec5ff8174de9f817d
gapinska <gapinska.mon@gmail.com>
2019-08-27 22:03:13 +0200

    loop conditionals 2 task

--- loops_conditionals.js
+++ loops_conditionals.js
@@ -66,6 +66,37 @@ arr.shift()
 arr.pop()
 console.log(arr)
 
+// TODO: 6. Stworz tablice z losowa parzysta dlugoscia elementow
+// i losowymi wartosciami w tablicy
+// stworz nowa tablice ktora zawiera 4 srodkowe elementy
+// console.log tej tablicy
+var length
+function checkMod(){
+    while((length%2)!=0){
+        length= Math.floor(Math.random() * 10 + 1);
+        return length;
+    }
+}
+var list=[]
+for (var i=0; i<length; i++){
+    var number= Math.floor(Math.random()*10+1);
+    list.push(number);
 
+}
+
+
+
+// TODO: 7. odszukaj index 9 w tablicy [1,2,3,4,5,6,7,8,9,10]
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(arr[9])
+
+
+// TODO: 8. stworz funkcje, ktora rekursywnie policzy do n
+// n ma byc parametrem tej funkcji
+function count(n){
+    for(var i =1; i<=n; i++){
+        console.log(i);
+    }
+}
 
 
---
+++ loops_conditionals_2.js
@@ -0,0 +1,39 @@
+var drinks = [
+    {name: 'Vodka', price: 19.99},
+    {name: 'Whisky', price: 40.00},
+    {name: 'Prosseco', price: 35.99},
+    {name: 'Beer', price: 3.20},
+    {name: 'Balieys', price: 50.20},
+    {name: 'Bacardi', price: 35.50},
+    {name: 'Vine', price: 16.50},
+    {name: 'Gin', price: 23.30}
+  ];
+
+/* TODO.1: Za pomocą pętli, wyświetl w konsoli nazwy drinków, których:
+    - cena jest niższa 30.00 */
+
+
+
+/* TODO.2: Za pomocą pętli, wyświetl w konsoli nazwy drinków, których:
+ - cena jest wyższa niż 20 ale jednocześnie niższa lub równa 40 */
+
+/* TODO.3: Za pomocą pętli, wyświetl w konsoli nazwy drinków, których:
+ - nazwa ma więcej niż 4 litery */
+
+/* TODO.4: Za pomocą pętli, wyświetl w konsoli nazwy drinków, których:
+ - nazwa zaczyna się na literkę "B" lub na literkę "V" */
+
+/* TODO.6: Za pomocą pętli, doklej do każdego drinka pole:
+ - isExpensive: true jeśli cena jest wyższa niż 30
+ - isExpensive: false jeśli cena jest niższa lub równa 30
+ - oczekiwany, przykładowy efekt:
+ { name: 'Bacardi', price: 35.50, isExpensive: true },
+ { name: 'Vine', price: 16.50,  isExpensive: false } */
+
+/* TODO.7: Za pomocą pętli, wyświetl w konsoli nazwe drinka:
+ - którego cena jest równa 50.20, a po wyświetleniu console.log przerwij natychmiast pętlę */
+
+/* TODO.8: Za pomocą pętli, wyświetl w konsoli następujacy tekst:
+    - "Cena _NAME_ wynosi _PRICE_"
+    - przeskocz powyższy console.log jeśli cena jest równa 3.20 lub 19.99
+ */
