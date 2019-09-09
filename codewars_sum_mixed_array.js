20b7b56dcf795f3a41c0dcd68c0156322c184a1e
Monika Gapińska <45268855+gapinska@users.noreply.github.com>
2019-09-09 15:20:31 +0200

    Create codewars_sum_mixed_array.js

---
+++ codewars_sum_mixed_array.js
@@ -0,0 +1,6 @@
+/*Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.*/
+
+
+let sumMix= x=>  x.reduce((acc, val)=>acc + Number(val), 0)
